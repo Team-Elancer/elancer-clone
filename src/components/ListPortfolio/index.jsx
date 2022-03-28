7bab76b47e74d3d9fed32557bbc740bad053dc70
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-03-28 13:22:11 -0400

    add: pages/프로젝트 보기

---
+++ src/components/ListPortfolio/index.jsx
@@ -0,0 +1,13 @@
+import * as S from './style';
+
+import ChoiceProject from 'components/Choice-Project';
+
+const ListPortfolio = () => {
+  return (
+    <S.FramePortfolio>
+      <ChoiceProject />
+    </S.FramePortfolio>
+  );
+};
+
+export default ListPortfolio;
