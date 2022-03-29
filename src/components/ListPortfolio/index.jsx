74c92005ee379193a629a0fb28768fde134ea4bd
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-03-29 17:18:27 -0400

    정리

--- src/components/ListPortfolio/index.jsx
+++ src/components/ListPortfolio/index.jsx
@@ -1,12 +1,6 @@
-import styled from 'styled-components';
 import * as S from './style';
 
 import Ecard from 'components/Project-Ecard';
-
-const DarkEcard = styled.div`
-  background-color: white;
-  color: black;
-`;
 
 const ListPortfolio = () => {
   return (
