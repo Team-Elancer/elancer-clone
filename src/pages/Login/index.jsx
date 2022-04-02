b038d3292e6f65a4b535244593815401ca406fea
Your Name <you@example.com>
2022-04-02 23:54:28 +0900

    로그인페이지 기초 및 메뉴바 css수정

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './style';
+import Header from 'layouts/Header';
+
+const Login = () => {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
+};
+
+export default Login;
