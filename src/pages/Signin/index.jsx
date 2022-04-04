13c21a5f4937bd0cd062a29bca596147e1e08d77
Your Name <you@example.com>
2022-04-05 02:46:04 +0900

    회원가입 1페이지 css완료

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import * as S from './style';
+import Logo from 'assets/images/elancer_logo.png';
+import Freelancer from 'assets/images/freelancer.png';
+import LogoWhite from 'assets/images/logo_white.png';
 import MenuBar from 'components/MenuBar';
 import GridBottom from 'components/Modal/GridBottom';
 import MainMenu from 'components/Modal/MainMenu';
@@ -18,6 +21,37 @@ const index = () => {
         </>
       )}
       <Header checkBool={checkBool} setCeckBool={setCeckBool} />
+      <S.SizeDiv>
+        <S.FlexDiv>
+          <S.H1>회원가입</S.H1>
+          <S.TextSpan>
+            <S.Padding>회원선택</S.Padding>
+            <S.BorderSpan>1 / 3</S.BorderSpan>
+          </S.TextSpan>
+        </S.FlexDiv>
+        <S.BlockDiv>
+          <S.BoxSizeDiv bgcolor="#f6f6f6">
+            <S.CenterDiv>
+              <S.ImgFlex>
+                <S.Img src={Logo} alt="logo" magin="7rem" />
+                <S.BigText>엔터프라이즈</S.BigText>
+              </S.ImgFlex>
+              <S.TopP color="black">기업 전용 가입하기</S.TopP>
+              <S.BottomP color="#707070">적합한 인재 찾기를 희망한 기업</S.BottomP>
+            </S.CenterDiv>
+          </S.BoxSizeDiv>
+          <S.BoxSizeDiv bgcolor="#3c3c3c">
+            <S.CenterDiv>
+              <S.ImgFlex>
+                <S.Img src={LogoWhite} alt="logo-white" />
+                <S.SmallImg src={Freelancer} alt="Freelancer" />
+              </S.ImgFlex>
+              <S.TopP color="white">프리랜서 가입하기</S.TopP>
+              <S.BottomP color="#a5a5a5">프로젝트를 찾는 프리랜서/크라우드 워커</S.BottomP>
+            </S.CenterDiv>
+          </S.BoxSizeDiv>
+        </S.BlockDiv>
+      </S.SizeDiv>
       <Footer />
       <GridBottom />
     </S.Container>
--- src/pages/Signin/style.js
+++ src/pages/Signin/style.js
@@ -1,6 +1,148 @@
 import styled from 'styled-components';
+import { MOBILE, TABLET, LAPTOP, DESKTOP } from 'utils/constants/responsive';
 
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+`;
+export const SizeDiv = styled.div`
+  width: 370px;
+  margin: 0 auto;
+  padding: 3rem 0 8rem 0;
+  @media ${TABLET} {
+    width: 747px;
+  }
+  @media ${LAPTOP} {
+    width: 800px;
+  }
+`;
+
+export const FlexDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 1rem;
+`;
+export const H1 = styled.h1`
+  font-size: 0.875rem;
+  font-weight: 800;
+  @media ${TABLET} {
+    font-size: 2.1rem;
+  }
+`;
+export const TextSpan = styled.span`
+  font-size: 0.8rem;
+  font-weight: 700;
+  padding-right: 1rem;
+  @media ${TABLET} {
+    padding-right: 2rem;
+  }
+`;
+export const Padding = styled.span`
+  padding-right: 1rem;
+`;
+export const BorderSpan = styled.span`
+  background-color: #e1e1e1;
+  border-radius: 15px;
+  padding: 0.4rem 0.7rem;
+  @media ${TABLET} {
+    font-size: 1.1rem;
+    border-radius: 25px;
+    padding: 0.5rem 1.1rem;
+  }
+`;
+export const BlockDiv = styled.div`
+  display: block;
+  @media ${TABLET} {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+export const BoxSizeDiv = styled.div`
+  width: 366px;
+  height: 10.5rem;
+  background-color: ${(props) => props.bgcolor};
+  border-radius: 10px;
+  margin-bottom: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media ${TABLET} {
+    width: 365px;
+    height: 15.5rem;
+    margin-right: 1rem;
+  }
+  @media ${LAPTOP} {
+    width: 411px;
+    height: 13rem;
+    margin-right: 1rem;
+    cursor: pointer;
+  }
+`;
+export const CenterDiv = styled.div`
+  width: 90%;
+  height: 85%;
+`;
+export const ImgFlex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding-top: 1.7rem;
+  @media ${TABLET} {
+    display: inline-block;
+  }
+  @media ${LAPTOP} {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+  }
+`;
+export const Img = styled.img`
+  width: 7rem;
+  margin-right: 0.9rem;
+  @media ${TABLET} {
+    margin-right: ${(props) => props.magin};
+    width: 10.1rem;
+  }
+  @media ${LAPTOP} {
+    margin-right: 0.8rem;
+  }
+`;
+export const SmallImg = styled.img`
+  width: 4rem;
+  @media ${TABLET} {
+    width: 5.5rem;
+  }
+`;
+export const BigText = styled.span`
+  font-size: 1.1rem;
+  font-weight: 800;
+  word-spacing: -0.2rem;
+  @media ${TABLET} {
+    font-size: 1.6rem;
+  }
+  @media ${LAPTOP} {
+    font-size: 1.4rem;
+  }
+`;
+export const TopP = styled.p`
+  padding-top: 3.5rem;
+  font-size: 0.9rem;
+  font-weight: 800;
+  word-spacing: -0.2rem;
+  color: ${(props) => props.color};
+  @media ${TABLET} {
+    font-size: 1rem;
+  }
+`;
+export const BottomP = styled.p`
+  font-size: 0.7rem;
+  font-weight: 800;
+  padding-top: 0.7rem;
+  color: ${(props) => props.color};
+  @media ${TABLET} {
+    font-size: 0.8rem;
+    padding-top: 1.5rem;
+  }
 `;
