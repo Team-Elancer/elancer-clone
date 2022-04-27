16f0cd88b13bdd01cf7bba73ffb89b0445d951b1
Your Name <you@example.com>
2022-04-28 05:49:40 +0900

    대시보 문의/요청 중

--- src/components/Myboard/LeftMenu/index.jsx
+++ src/components/Myboard/LeftMenu/index.jsx
@@ -13,24 +13,24 @@ const LeftMenu = () => {
       <S.MenuDiv>
         <S.UlTag>
           <Link to="/dashboard/enterprise">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/enterprise' ? '#B2B2B2' : 'white'}>나의 현황</S.MenuLi>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/enterprise' ? '#f2f2f2' : 'white'}>나의 현황</S.MenuLi>
           </Link>
           <Link to="/dashboard/profile">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/profile' ? '#B2B2B2' : 'white'}>프로필 관리</S.MenuLi>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/profile' ? '#f2f2f2' : 'white'}>프로필 관리</S.MenuLi>
           </Link>
           <Link to="/dashboard/project">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/project' ? '#B2B2B2' : 'white'}>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/project' ? '#f2f2f2' : 'white'}>
               등록한 프로젝트
             </S.MenuLi>
           </Link>
           <Link to="/dashboard/scrap">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/scrap' ? '#B2B2B2' : 'white'}>인재 스크랩</S.MenuLi>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/scrap' ? '#f2f2f2' : 'white'}>인재 스크랩</S.MenuLi>
           </Link>
           <Link to="/dashboard/contact">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/contact' ? '#B2B2B2' : 'white'}>문의/요청</S.MenuLi>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/contact' ? '#f2f2f2' : 'white'}>문의/요청</S.MenuLi>
           </Link>
           <Link to="/dashboard/account">
-            <S.MenuLi bgColor={lcation.pathname === '/dashboard/account' ? '#B2B2B2' : 'white'}>이랜서 계정</S.MenuLi>
+            <S.MenuLi bgColor={lcation.pathname === '/dashboard/account' ? '#f2f2f2' : 'white'}>이랜서 계정</S.MenuLi>
           </Link>
         </S.UlTag>
       </S.MenuDiv>
--- src/pages/DashBoard/Contact/style.js
+++ src/pages/DashBoard/Contact/style.js
@@ -5,3 +5,86 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const FlexDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media ${TABLET} {
+    margin-top: 2.2rem;
+  }
+  @media ${LAPTOP} {
+  }
+`;
+export const H1 = styled.h1`
+  font-size: 0.8rem;
+  font-weight: 800;
+  cursor: ${(props) => props.cursor};
+  @media ${TABLET} {
+    font-size: ${(props) => props.size};
+  }
+  @media ${LAPTOP} {
+  }
+`;
+
+export const OverflowDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  padding: 2rem 0;
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+  @media ${TABLET} {
+    padding: 3rem 0;
+  }
+  @media ${LAPTOP} {
+  }
+`;
+
+export const WidthDiv = styled.div`
+  width: 970px;
+  height: 205px;
+  display: flex;
+  align-items: center;
+  overflow: auto;
+  @media ${TABLET} {
+    width: 100%;
+  }
+  @media ${LAPTOP} {
+  }
+`;
+
+export const EcardDiv = styled.div`
+  width: 224px;
+  height: 176px;
+  padding: 10px;
+  background-color: #f2f2f2;
+  border-radius: 0.7rem;
+  margin-right: ${(props) => props.mobileRight};
+  @media ${TABLET} {
+    padding: 17.5px;
+    width: 200px;
+    height: 183px;
+    margin-right: ${(props) => props.tabletRight};
+  }
+  @media ${LAPTOP} {
+  }
+`;
+export const WhiteEcardDiv = styled.div`
+  width: 224px;
+  height: 176px;
+  padding: 10px;
+  background-color: white;
+  border: 5px solid #f2f2f2;
+  border-radius: 0.7rem;
+  margin-right: 1.5rem;
+  @media ${TABLET} {
+    padding: 17.5px;
+    width: 200px;
+    height: 183px;
+    margin-right: 1rem;
+  }
+  @media ${LAPTOP} {
+  }
+`;
