58e9962b30dab3abfc0590477dcb99f8af2da6d5
Your Name <you@example.com>
2022-04-30 04:47:49 +0900

    contact 모달 mobile css

--- src/components/Modal/DashBoardContact/index.jsx
+++ src/components/Modal/DashBoardContact/index.jsx
@@ -16,23 +16,31 @@ const ContactModal = ({ setModalBool }) => {
         <S.H1>1:1 문의/요청</S.H1>
       </S.FlexDiv>
       <S.FlexInputDiv top="0.3rem">
-        <S.H2 right="6rem">성명</S.H2>
+        <S.H2 width="200px" tabletWidth="151px">
+          성명
+        </S.H2>
         <S.Input />
       </S.FlexInputDiv>
       <S.FlexInputDiv top="0.8rem">
-        <S.H2 right="5.2rem">휴대폰</S.H2>
+        <S.H2 width="198px" tabletWidth="150px">
+          휴대폰
+        </S.H2>
         <S.Input />
       </S.FlexInputDiv>
       <S.FlexInputDiv top="0.8rem">
-        <S.H2 right="5.2rem">이메일</S.H2>
+        <S.H2 width="198px" tabletWidth="150px">
+          이메일
+        </S.H2>
         <S.Input />
       </S.FlexInputDiv>
       <S.FlexInputDiv top="0.8rem">
-        <S.H2 right="6rem">제목</S.H2>
+        <S.H2 width="200px" tabletWidth="151px">
+          제목
+        </S.H2>
         <S.Input placeholder="제목입니다." />
       </S.FlexInputDiv>
       <S.FlexInputDiv top="0.9rem">
-        <S.H2 width="100px" top="-2rem">
+        <S.H2 width="85px" tabletWidth="140px" top="-2rem">
           문의내용
         </S.H2>
         <S.Textarea placeholder="문의내용을 입력해주세요." />
