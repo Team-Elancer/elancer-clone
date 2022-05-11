b09c31ea4b5c6ccff435fe09a157f2370bd9dffc
heyfranksmile <heyfranksmile@gmail.com>
2022-05-11 14:46:55 +0900

    add: MyBoard + Private Route & fix: Footer 스타일 수정

---
+++ src/pages/MyBoardFreelancer/index.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import * as S from './style';
+
+import GridBottom from 'components/Modal/GridBottom';
+import LeftMenuMyBoard from 'components/Myboard/LeftMenu/Freelancer';
+
+import Footer from 'layouts/Footer';
+import Header from 'layouts/Header';
+
+const MyBoardFreelancer = () => {
+  return (
+    <S.Container>
+      <Header freelancerBoard />
+      <LeftMenuMyBoard />
+      <Footer />
+      <GridBottom />
+    </S.Container>
+  );
+};
+
+export default MyBoardFreelancer;
