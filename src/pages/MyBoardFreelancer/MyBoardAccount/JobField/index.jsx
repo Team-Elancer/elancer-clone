432e39f431408590b3604802e9325724be63c367
heyfranksmile <heyfranksmile@gmail.com>
2022-06-07 18:08:32 +0900

    add: pages/myboard/account - job field & added Login function

--- src/pages/MyBoardFreelancer/MyBoardAccount/JobField/index.jsx
+++ src/pages/MyBoardFreelancer/MyBoardAccount/JobField/index.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react';
 import * as S from './style';
 
-const JobField = ({ workType, workTypeField, setWorkTypeField }) => {
+const JobField = ({ workType, workTypeField, setWorkTypeField, fieldDataBase }) => {
+  // Filter the database index for CSS(active)
+  const filteredIndex = workTypeField.map((typeIndex) => fieldDataBase.indexOf(typeIndex));
+
   const handleThreeJobField = (e) => {
     const jobType = e.target.id;
+    const jobEnumType = e.target.htmlFor;
 
-    if (jobType && !workTypeField.includes(jobType)) {
+    // Handle clicked data
+    if (jobType && !workTypeField.includes(jobEnumType)) {
       if (workTypeField.length < 3) {
-        setWorkTypeField((prev) => [...prev, jobType]);
+        setWorkTypeField((prev) => [...prev, jobEnumType]);
       } else {
         alert('3개 업무만 가능해요');
       }
       return;
     }
 
-    if (jobType && workTypeField.includes(jobType)) {
-      setWorkTypeField((prev) => prev.filter((type) => type !== jobType));
+    // Unclick jobEnumType
+    if (workTypeField.includes(jobEnumType)) {
+      setWorkTypeField((prev) => prev.filter((type) => type !== jobEnumType));
     }
   };
+
   return (
     <S.FrameOptions>
-      {workType.map((type) => (
+      {workType.map((type, index) => (
         <S.ContainerOptions key={type}>
           <S.ContainerList>
             <S.ButtonLabel
               id={type}
+              htmlFor={fieldDataBase[index]}
               onClick={(e) => {
                 handleThreeJobField(e);
               }}
-              active={workTypeField.includes(type)}
+              // Handle CSS: compare index
+              active={filteredIndex.includes(index)}
             >
               {type}
             </S.ButtonLabel>
