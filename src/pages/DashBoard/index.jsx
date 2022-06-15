bf126cbae40bf73b4a73445a08496849e011e475
Your Name <you@example.com>
2022-06-15 17:16:56 +0900

    react spin 및 usecontext dashboard

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -1,11 +1,45 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import * as S from './style';
+
 import GridBottom from 'components/Modal/GridBottom';
 import LeftMenu from 'components/Myboard/LeftMenu';
 import Footer from 'layouts/Footer';
 import CompanyHeader from 'layouts/Header/Company';
 
 const Dashboard = () => {
+  const [Datas, setDatas] = useState('');
+  const [axiosUrl, setaxiosUrl] = useState('');
+
+  const authAxios = axios.create({
+    baseURL: 'http://ec2-13-209-114-196.ap-northeast-2.compute.amazonaws.com:8080',
+    headers: {
+      Authorization: `${window.localStorage.accessToken}`,
+    },
+  });
+
+  const fetchData = async () => {
+    try {
+      if (axiosUrl) {
+        const res = await authAxios(axiosUrl);
+        const data = await res.data;
+        setDatas(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  console.log(Datas);
+
+  useEffect(() => {
+    if (axiosUrl) {
+      fetchData();
+    }
+  }, [axiosUrl]);
+
   return (
     <S.Container>
       <CompanyHeader width="1224px" />
@@ -13,7 +47,7 @@ const Dashboard = () => {
         <S.FlexDiv>
           <LeftMenu />
           <S.BoardDiv>
-            <Outlet />
+            <Outlet context={[Datas, setDatas, axiosUrl, setaxiosUrl, fetchData]} />
           </S.BoardDiv>
         </S.FlexDiv>
       </S.SizeDiv>
