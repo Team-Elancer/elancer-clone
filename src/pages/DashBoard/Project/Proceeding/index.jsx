fe1d6dbee97b03f1ce237cba13389edfdffccfae
sni424 <jojonghyeok424@gmail.com>
2022-07-05 01:59:37 +0900

    모든 프로젝트 리스트 페이지 배열 순서 변경

--- src/pages/DashBoard/Project/Proceeding/index.jsx
+++ src/pages/DashBoard/Project/Proceeding/index.jsx
@@ -6,23 +6,26 @@ import ProjectListSkeleton from 'components/Skeleton/ProjectList';
 
 const ProjectProceeding = ({ Datas, setNewAxiosUrl, newAxiosUrl, newReloading, setNewReloading }) => {
   const [newProcessing, setNewProcessing] = useState('');
-
-  console.log(newProcessing);
+  const [newProcessings, setNewProcessings] = useState('');
 
   useEffect(() => {
     setNewAxiosUrl('/processing-project');
     if (newAxiosUrl === '/processing-project') {
       setNewProcessing(Datas);
     }
-  }, [Datas]);
+    if (newProcessing.length > 0) {
+      const reverse = newProcessing.reverse();
+      setNewProcessings(reverse);
+    }
+  }, [Datas, newProcessing]);
 
   return (
     <>
       <S.H1 top="4rem" laptoptop="4rem">
         진행중 프로젝트 리스트 ({newProcessing !== '' && newProcessing.length})
       </S.H1>
-      {newProcessing ? (
-        newProcessing.map((data) => {
+      {newProcessings ? (
+        newProcessings.map((data) => {
           return (
             <ProjectList
               data={data}
